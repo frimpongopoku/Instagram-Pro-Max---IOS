686d43cdcea84791f1715ed3cd5182d0afb63c65
frimpongopoku <pongofrimi@gmail.com>
2020-12-31 16:28:05 +0000

    Initial Commit

---
+++ Shared/Instagram_Pro_MaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_Pro_MaxApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 31/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_Pro_MaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
